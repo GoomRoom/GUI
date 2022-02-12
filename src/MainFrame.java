be68dcdd0b79aa1d31d8f3f08d2bcacbab5fbde9
Derek Sanchez <98922114+GoomRoom@users.noreply.github.com>
2022-02-12 23:32:30 +0000

    TEST

--- src/MainFrame.java
+++ src/MainFrame.java
@@ -73,6 +73,7 @@ public class MainFrame extends javax.swing.JFrame {
         /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
          * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
          */
+         //Test
         try {
             for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
                 if ("Nimbus".equals(info.getName())) {
